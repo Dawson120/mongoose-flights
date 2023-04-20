32f79769fa151217237902681cd1dcb829a91ea1
Dawson Myers <Djmyers2002@gmail.com>
2023-04-20 01:47:23 -0400

    Step 1 Complete

---
+++ controllers/flights.js
@@ -0,0 +1,84 @@
+import { Flight } from "../models/flight.js";
+
+function newFlight (req, res) {
+  res.render('flights/new', {
+    title: 'New Flight'
+  })
+}
+
+function create(req, res) {
+  Flight.create(req.body)
+    .then(flight => {
+      res.redirect('/flights')
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/flights/new')
+    })
+}
+
+function index (req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights: flights,
+      title: 'All Flights',
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
+
+function deleteFlight(req, res) {
+  Flight.findByIdAndDelete(req.params.flightId)
+  .then(flight => {
+    res.redirect('/flights')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
+}
+
+function show(req, res) {
+  Flight.findById(req.params.flightId)
+  .then (flight => {
+    res.render('flights/show', {
+      flight: flight,
+      title: "About Flight",
+    })
+  })
+}
+
+function edit(req, res) {
+  Flight.findById(req.params.flightId)
+  .then (flight => {
+    res.render('flights/edit', {
+      flight: flight,
+      title: "Edit Flight"
+    })
+  })
+}
+
+function update(req, res) {
+  Flight.findByIdAndUpdate(req.params.flightId, req.body, {new: true})
+  .then(flight => {
+    res.redirect(`/flights`)
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+export {
+  newFlight as new,
+  create,
+  index,
+  show,
+  deleteFlight as delete,
+  edit,
+  update,
+}
---
+++ routes/flights.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import * as flightsCtrl from '../controllers/flights.js'
+
+const router = Router()
+
+router.get('/', flightsCtrl.index)
+router.get('/new', flightsCtrl.new)
+router.get('/:flightId', flightsCtrl.show)
+router.post('/', flightsCtrl.create)
+router.get('/:flightId/edit', flightsCtrl.edit)
+router.put('/:flightId', flightsCtrl.update)
+router.delete('/:flightId', flightsCtrl.delete)
+
+export { router }
